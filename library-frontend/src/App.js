223daf6a658563de7ba37db134e07ccdc6ef4938
jusa-a <jusa.annevirta@aalto.fi>
2023-07-26 22:30:38 +0300

    8.8: Authors view

--- library-frontend/src/App.js
+++ library-frontend/src/App.js
@@ -3,24 +3,46 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+    query {
+        allAuthors {
+            name
+            born
+            bookCount
+            id
+        }
+    }
+`
+
 const App = () => {
-  const [page, setPage] = useState('authors')
+    const [page, setPage] = useState('authors')
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
-      </div>
+    const result = useQuery(ALL_AUTHORS)
 
-      <Authors show={page === 'authors'} />
+    if (result.loading) {
+        return <div>loading...</div>
+    }
 
-      <Books show={page === 'books'} />
+    return (
+        <div>
+            <div>
+                <button onClick={() => setPage('authors')}>authors</button>
+                <button onClick={() => setPage('books')}>books</button>
+                <button onClick={() => setPage('add')}>add book</button>
+            </div>
 
-      <NewBook show={page === 'add'} />
-    </div>
-  )
+            <Authors
+                show={page === 'authors'}
+                authors={result.data.allAuthors}
+            />
+
+            <Books show={page === 'books'} />
+
+            <NewBook show={page === 'add'} />
+        </div>
+    )
 }
 
 export default App
